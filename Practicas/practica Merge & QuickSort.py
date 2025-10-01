a02caf7f1feb1df00209dd418663807474866fa6
Angel Villalvazo <angel.villalvazo5367@alumnos.udg.mx>
2025-10-01 08:45:51 -0600

    Práctica durante clase, divide y vencerás

---
+++ Practicas/practica Merge & QuickSort.py
@@ -0,0 +1,52 @@
+def merge_sort(array):
+    if len(array) > 1:
+        mid = len(array)//2
+        left = array[:mid]
+        right = array[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        merge_a(array, left, right)
+    return array
+
+def merge_a(array, left, right):
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        array[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        array[k] = right[j]
+        j += 1
+        k += 1
+
+# Quick Sort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+arr = [7, 38, 27, 43, 3, 9, 82, 10]
+sorted_arr = merge_sort(arr)
+print("Ordenado por merge sort: ",sorted_arr)
+sorted_arr = quick_sort(arr)
+print("Ordenado por quick sort: ",sorted_arr)
